7afe68cea5337cdc6db428caae5e4ccaddd53da0
razzouko <orazzouk8@boscdelacoma.cat>
2023-01-08 16:43:42 +0100

    arreglar validators i afegir altertes d'errors

--- src/app/projecte/components/criteri/criteri.component.ts
+++ src/app/projecte/components/criteri/criteri.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { FormBuilder, FormGroup, FormArray ,Validators, FormControl } from '@angular/forms';
+import { FormBuilder, FormGroup, FormArray, Validators, FormControl } from '@angular/forms';
 
 @Component({
   selector: 'app-criteri',
@@ -9,46 +9,53 @@ import { FormBuilder, FormGroup, FormArray ,Validators, FormControl } from '@ang
 export class CriteriComponent implements OnInit {
 
   formCriteri!: FormGroup;
-  creacioCriteris : boolean = true;
-  creacioValoracions : boolean = false;
+  creacioCriteris: boolean = true;
+  creacioValoracions: boolean = false;
 
-  
 
-  constructor( private fb : FormBuilder) { }
+
+  constructor(private fb: FormBuilder) { }
 
   ngOnInit(): void {
 
     this.formCriteri = this.fb.group({
-      titolCriteri : ['' , Validators.required],
-      valoracions : this.fb.array([])
+      titolCriteri: ['', Validators.minLength(5)],
+      valoracions: this.fb.array([])
     })
   }
 
 
-  afegirValoracio(){
+  afegirValoracio() {
     this.valoracions().push(this.novaValoracio());
   }
 
 
-  novaValoracio() : FormGroup{
+  novaValoracio(): FormGroup {
     return this.fb.group({
-      titolValoracio : ['' , Validators.required],
-      numero : ['' , Validators.required] //  es guarda en format string, al construir la classe es fa el parse a number
+      titolValoracio: ['', Validators.minLength(5)],
+      numero: ['', Validators.pattern('[0-9]+')] //  es guarda en format string, al construir la classe es fa el parse a number
     });
   }
 
-  valoracions(){
+  valoracions() {
     return this.formCriteri.get("valoracions") as FormArray; //  pot donar problemes de al intentar accedir als seus valors
   }
 
 
-  guardarCriteri(){
-    let formobj = { 
-                    titol : this.formCriteri.value.titolCriteri, 
-                    valors : this.formCriteri.value.valoracions
-                  };
-    localStorage.setItem(formobj.titol , JSON.stringify(formobj.valors));
+  guardarCriteri() {
+
+    if(this.formCriteri.invalid){
+      alert("Hi han camps invalids");
+    }else if(this.formCriteri.value.valoracions.length <= 0){
+      alert("Falten valoracions");
+    }else{
+    let formobj = {
+      titol: this.formCriteri.value.titolCriteri,
+      valors: this.formCriteri.value.valoracions
+    };
+    localStorage.setItem(formobj.titol, JSON.stringify(formobj.valors));
     window.location.reload();
+    }
   }
 
 
--- src/app/projecte/components/rubrica/rubrica.component.ts
+++ src/app/projecte/components/rubrica/rubrica.component.ts
@@ -1,7 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { criteri } from '../../model/entitats/implementacions/criteri';
 import { compareNums } from '../../helps/helpers';
-import { valoracio } from '../../model/entitats/implementacions/valoracio';
+
 @Component({
   selector: 'app-rubrica',
   templateUrl: './rubrica.component.html',
@@ -10,91 +10,114 @@ import { valoracio } from '../../model/entitats/implementacions/valoracio';
 export class RubricaComponent implements OnInit {
 
   criteris: Array<criteri> = [];
-  valoracionsNumDiff : Array<number> = [];
+  valoracionsNumDiff: Array<number> = [];
   eleccions = new Map();
-  sumaEleccions = [0 , 0]; // [suma de les eleccions, suma total]
+  sumaEleccions = [0, 0]; // [suma de les eleccions, suma total]
+  mostrarResultat = false;
+  hiHanDades = (Object.keys(localStorage).length > 0) ? true : false ;
 
   constructor() { }
 
   ngOnInit(): void {
+
     this.valoracionsNumDiff = this.obtenirNumerosDiff();
     this.obtenirObjectesCriteri();
+    this.iniciarMapa();
   }
 
-  obtenirObjectesCriteri(){
+  obtenirObjectesCriteri() {
 
     let localStObj = this.objectesDeLocalStorage();
 
     for (let i = 0; i < localStObj.length; i++) {
-       let objCriteri = new criteri(<string> localStObj[i][0], localStObj[i][1]); // (titol criteri , valoracions[])
-       this.criteris.push(objCriteri);
+      let objCriteri = new criteri(<string>localStObj[i][0], localStObj[i][1]); // (titol criteri , valoracions[])
+      this.criteris.push(objCriteri);
     }
   }
 
-  objectesDeLocalStorage(){
+  objectesDeLocalStorage() {
 
     let values = [];
     let keys = Object.keys(localStorage);
     let i = keys.length;
 
-    while ( i-- ) {
-      if(keys[i] !== null) {
-        values.push([keys[i] , JSON.parse(<string> localStorage.getItem(keys[i]))]) ;
+    while (i--) {
+      if (keys[i] !== null) {
+        values.push([keys[i], JSON.parse(<string>localStorage.getItem(keys[i]))]);
       }
     }
     return values;
   }
 
-  obtenirNumerosDiff(){
+  obtenirNumerosDiff() {
     let dadesCriteris = this.objectesDeLocalStorage();
-    let diferents : number[] = [];
+    let diferents: number[] = [];
     for (let i = 0; i < dadesCriteris.length; i++) {
       let valoracions = dadesCriteris[i][1];
       this.sumaEleccions[1] += this.valoracioMesGran(valoracions); // aprofitant el bucle que recorre les valoracions calculem el valor maxim de la rubrica
       for (let j = 0; j < valoracions.length; j++) {
-        if(!diferents.includes(parseInt(valoracions[j].numero))){
-              diferents.push(parseInt(valoracions[j].numero))
-          }
+        if (!diferents.includes(parseInt(valoracions[j].numero))) {
+          diferents.push(parseInt(valoracions[j].numero))
+        }
       }
     }
     return diferents.sort(compareNums);
   }
 
-  valoracioMesGran(dadesCriteri : any[]) : number{
-    let max : number = 0;
+  valoracioMesGran(dadesCriteri: any[]): number {
+    let max: number = 0;
     for (let i = 0; i < dadesCriteri.length; i++) {
-        if(dadesCriteri[i].numero > max){
-          max = parseInt(dadesCriteri[i].numero);
-        }
+      if (dadesCriteri[i].numero > max) {
+        max = parseInt(dadesCriteri[i].numero);
+      }
     }
     return max;
   }
 
-  seleccionarValoracio( titolCriteri : string  , num : number){
-        this.eleccions.set(titolCriteri , num);
-  }
+  iniciarMapa() {
 
-  esSeleccionat(titolCriteri : string , num : number){
+    let keys = Object.keys(localStorage);
 
-      let seleccio = this.eleccions.get(titolCriteri);
-      if(seleccio == num)
-        return true;
-      else  
-        return false;
-  }
-
-
-  sumarEleccions(){
-
-    let total = 0;
-    let numero = "";
-    for (let [key , value] of this.eleccions) {
-        numero = <string> value;
-        this.sumaEleccions[0] += parseInt(numero);
+    for (let i = 0; i < keys.length; i++) {
+        this.eleccions.set(keys[i] , -1);
     }
 
-    return total;
   }
-  
+
+  seleccionarValoracio(titolCriteri: string, num: number) {
+    this.eleccions.set(titolCriteri, num);
+  }
+
+  esSeleccionat(titolCriteri: string, num: number) {
+
+    let seleccio = this.eleccions.get(titolCriteri);
+    if (seleccio == num)
+      return true;
+    else
+      return false;
+  }
+
+
+  calcularResultat() {
+
+    let faltaCriteri = false;
+    let numero = "";
+    for (let [key, value] of this.eleccions) {
+      numero = <string>value;
+      if(parseInt(numero) == -1){
+        this.sumaEleccions[0] = 0;
+        faltaCriteri = true;
+        break;
+      }
+      this.sumaEleccions[0] += parseInt(numero);
+    }
+
+    if(faltaCriteri){
+      alert("No es pot calcular, falta un criteri per seleccionar")
+    }else{
+      this.mostrarResultat = true;
+    }
+  }
+
 }
 
